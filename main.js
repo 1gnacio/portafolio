8cfa4214932400bddd4e39f98750271b3e77508f
Nacho <ignacio5garcia@hotmail.com>
2020-12-22 19:41:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+    }
+});
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+
+    //FILTRAR POR CATEGORIA
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach( (elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach( (enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+        
+            const categoria = evento.target.innerHTML.toLowerCase();
+            categoria === 'show all' ? grid.filter(`[data-categoria]`) : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+
+    //BARRA DE BUSQUEDA: LISTENER
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+    });
+
+    //IMAGENES: LISTENER
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach( (elemento) => {
+        const ruta = elemento.getAttribute('src');
+        const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+        
+        elemento.addEventListener('click', () => {
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src = ruta;
+            document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+        });
+    });
+
+    //BOTON CERRAR: LISTENER
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+        overlay.classList.remove('activo');
+    });
+
+    //OVERLAY: LISTENER
+    overlay.addEventListener('click', (evento) => {
+        evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+    })
+});
